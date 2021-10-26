dd81490b1ae3e9a65004c8849bbed2af154245ba
Pulkit Chandra <pulkitchandra89@gmail.com>
2021-10-27 03:43:41 +0530

    Learning Java, initial commit

---
+++ java/HelloWorld.java
@@ -0,0 +1,6 @@
+class Application {
+  public static void main(String args[]) {
+    // Simple Program
+    System.out.println("Hello World");
+  }
+}
---
+++ java/SampleApplet.java
@@ -0,0 +1,8 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class SampleApplet extends Applet {
+  public void paint(Graphics g) {
+    g.drawString("Hello Ignou !", 50, 25);
+  }
+}
