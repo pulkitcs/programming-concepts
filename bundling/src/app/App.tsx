feb728fe2b14edfda4f138fcd0ca87bf0799f40c
Pulkit Chandra <pulkitchandra89@gmail.com>
2022-07-28 23:29:37 +0530

    Removed deprecated repo

--- bundling/src/app/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Welcome from './Welcome'
-
-const App: React.FC = () => <div>works good oh god, <Welcome /></div>;
-
-export default App;
---
+++ readme.md
@@ -0,0 +1,5 @@
+## Learning C and Java
+
+- Contains code of common C datastructures 
+- Common C alogoriths
+- Java practice and functionality examples
