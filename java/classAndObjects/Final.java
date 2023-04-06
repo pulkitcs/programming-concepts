aa90e29b1f1e7f40a263eaceb00490dfac6c63e4
Pulkit Chandra <pulkit.cs@gmail.com>
2023-04-07 01:08:58 +0530

    Files moved to a different folder

---
+++ java/classAndObjects/Final.java
@@ -0,0 +1,13 @@
+package classAndObjects;
+
+public class Final {
+  public static void main(String[] args) {
+    final int a;
+    a = 100; 
+    a = 120; // will throw compile time error
+    System.out.println("this works");
+  }
+}
+
+
+
