fad44f72f28fd454849430ab45a78fb60a9613b6
Pulkit Chandra <pulkit.cs@gmail.com>
2024-01-17 02:58:22 +0530

    ADD: Implement delete function

--- web/ds/bst.ts
+++ web/ds/bst.ts
@@ -1,19 +1,25 @@
 interface INode<dataType> {
   data: dataType;
+  //prev?:dataType | undefined;
   left: INode<dataType> | undefined;
   right: INode<dataType> | undefined
 }
 
 interface IBinarySearchTree<dataType> {
   insert(data: dataType): INode<dataType> | undefined;
-  //delete(data: dataType): INode<dataType> | null;
+  delete(data: dataType): INode<dataType> | undefined;
   // inorder(node: INode<dataType> |  undefined): void;
   // preorder(node: INode<dataType> |  undefined): void;
   // postorder(node: INode<dataType> |  undefined): void;
-  search(data: dataType): INode<dataType> | undefined
+  search(data: dataType): searchResult<dataType> 
 }
 
 type comparator<dataType> = (a: dataType, b: dataType) => boolean;
+
+type searchResult<dataType> = {
+  prev: INode<dataType> | undefined;
+  node: INode<dataType> | undefined;
+}
 
 class Nodes<dataType> implements INode<dataType> {
   left = undefined;
@@ -69,19 +75,41 @@ class BinarySearchTree<dataType> implements IBinarySearchTree<dataType> {
     return node;
   }
 
-  search(value: dataType): INode<dataType> | undefined {
-    const searchNode = (node: INode<dataType>): INode<dataType> | undefined => {
+  search(value: dataType): searchResult<dataType> {
+    const searchNode = (node: INode<dataType>, prev: INode<dataType> | undefined = undefined): searchResult<dataType> => {
       const { left, right, data: nodeValue } = node;
       const shouldAddtoLeft = this.compare(value, nodeValue);
 
-      if(nodeValue === value) return node;
-      else if ( shouldAddtoLeft && left) return searchNode(left);
-      else if (!shouldAddtoLeft && right) return searchNode(right);
-      else if (!left && !right) return undefined;
+      if(nodeValue === value) return ({node, prev});
+      else if ( shouldAddtoLeft && left) return searchNode(left, node);
+      else if (!shouldAddtoLeft && right) return searchNode(right, node);
+      else if (!left && !right) return ({prev: undefined, node: undefined});
+
+      return ({prev: undefined, node: undefined});
     } 
 
-    if(this?.root) return searchNode(this.root);
-    else return undefined;
+    if(this?.root) return searchNode(this.root, undefined);
+    else return ({prev: undefined, node: undefined});
+  }
+
+  delete(value: dataType): INode<dataType> | undefined {
+    const {prev, node} = this.search(value);
+
+    if(!node) return undefined;
+    else if(!prev) return this.root = undefined;
+
+    if(!node.left && !node.right) {
+      const deleteParentFromLeft = this.compare(value, prev.data); 
+
+      if(deleteParentFromLeft && prev.left) {
+        delete prev.left
+      }
+      else if (!deleteParentFromLeft && prev.right){
+        delete prev.right;
+      }
+    }
+
+    return node;
   }
 }
 
@@ -98,6 +126,7 @@ bst.insert(7);
 bst.insert(14);
 bst.insert(13);
 
-console.log(bst);
+console.log("Search result", bst.search(13));
+console.log("deleted node", bst.delete(13));
 
-console.log("Search result", bst.search(1));
+console.log(bst);
