3b3f6324379bfac390bd79d0127d81a120352854
Pulkit Chandra <pulkit.cs@gmail.com>
2024-02-10 21:26:01 +0530

    New logic addition

--- web/conceptsAndAlgo/anagram.ts
+++ web/conceptsAndAlgo/anagram.ts
@@ -11,6 +11,48 @@ function checkAnagram(str1: string , str2: string): boolean {
   return sortedStr1 === sortedStr2;
 };
 
-console.time()
-console.log(checkAnagram('silent', 'listen'));
-console.timeEnd()
+console.time('smart')
+checkAnagram('silent', 'listen');
+console.timeEnd('smart');
+
+
+
+
+const input = ["eat", "ate", "tea", "himanshu", "ear","team", "meat", "mate", "are"];
+//[["eat", "ate", "tea"], ["himanshu"], ["team", "meat", "mate"],["are", "ear"]]
+
+
+
+function checkAnagrams(input: string[]): string[][] {
+  const output: string[][] = [];
+  const map: { [key: string]: boolean} = {};
+  
+   input.forEach((word, index) => {
+     const seq = map[index] ? [] : [word];
+     map[index] = true;
+     
+     for(let i = 0; i < input.length; i++) {
+       const currentWord = input[i];
+       
+       if(word.length !== currentWord.length || index === i || map[i]) {
+         continue;
+       };
+
+       const result = word.split('').every((c) => currentWord.indexOf(c) > -1);
+
+       if(result) {
+         seq.push(currentWord);
+         map[i] = true;
+       }
+     }
+
+     seq.length && output.push(seq);
+   });
+
+  return output;
+
+};
+
+console.time('lazy');
+checkAnagrams(input);
+console.timeEnd('lazy');
