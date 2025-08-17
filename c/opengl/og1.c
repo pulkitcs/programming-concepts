654a163537d93ec7e80cecd6c6f39ed241ec14da
Pulkit Chandra <pulkit.cs@gmail.com>
2025-08-17 20:52:53 +0530

    OpenGL programs

---
+++ c/opengl/og1.c
@@ -0,0 +1,53 @@
+// opengl.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <windows.h>
+#include <gl/gl.h>
+#include <gl/glu.h>
+#include <gl/glut.h>
+
+
+void myInit(void) {
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glPointSize(4.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, 800.0, 0.0, 600.0);
+}
+
+void myDisplay(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    glVertex2i(100, 300);
+    glVertex2i(400, 300);
+    glVertex2i(600, 150);
+    glVertex2i(400, 100);
+
+    glEnd();
+    glFlush();
+}
+
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("OpenGL Window");
+    glutDisplayFunc(myDisplay);
+    myInit();
+    glutMainLoop();
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
